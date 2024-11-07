f56c35899f55de410ce77bea2e7ffc770b32e97d
Parth Maha <parthmaha9@gmail.com>
2024-11-07 13:39:06 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -467,10 +467,6 @@ export default function App() {
                 <span className="text-gray-300 text-lg">Java</span>
               </div>
               <div className="bg-zinc-900 rounded-lg p-6 flex flex-col items-center justify-center aspect-square">
-                <SiSpringboot className="w-16 h-16 mb-4 p-2 text-[#6DB33F]" />
-                <span className="text-gray-300 text-lg">Spring Boot</span>
-              </div>
-              <div className="bg-zinc-900 rounded-lg p-6 flex flex-col items-center justify-center aspect-square">
                 <SiFlutter className="w-16 h-16 mb-4 p-2 text-[#02569B]" />
                 <span className="text-gray-300 text-lg">Flutter</span>
               </div>
